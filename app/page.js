2b35e16b2f9583a28773a52a0305d51d5645de65
bayusatmoko <bsatmoko@gmail.com>
2023-12-05 01:05:22 +0700

    finish skeleton

--- app/page.js
+++ app/page.js
@@ -1,9 +1,29 @@
 export default function Home() {
-    return (
-      <div class="grid-container">
-        <div class="menu"></div>
-        <div class="main-view"></div>
-        <div class="bottom-menu"></div>
+  return (
+    <div className="grid-container">
+      <div className="menu">
+        <ul>
+          <li>
+            <a
+              href="#"
+              class="hover:font-black dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="hover:font-black dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Search
+            </a>
+          </li>
+        </ul>
       </div>
-    );
+      <div className="main-view"></div>
+      <div className="bottom-menu"></div>
+      <div className="now-playing-bar"></div>
+    </div>
+  );
 }
