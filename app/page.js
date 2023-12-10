beb0f6eda17b69e35067ee187e8a548d08739fe9
bayusatmoko <bsatmoko@gmail.com>
2023-12-10 22:42:48 +0700

    make banner clickable

--- app/page.js
+++ app/page.js
@@ -3,15 +3,17 @@ import { useEffect, useState } from "react";
 import ReactAudioPlayer from "react-audio-player";
 import Image from "next/image";
 
-const S3_BASE = "https://songssongs.s3.ap-southeast-1.amazonaws.com"
+const S3_BASE = "https://songssongs.s3.ap-southeast-1.amazonaws.com";
 
 export default function Home() {
   const [songs, setSongs] = useState([]);
-  const [currentSong, setCurrentSong] = useState('https://songssongs.s3.ap-southeast-1.amazonaws.com/nadhif.mp3')
+  const [currentSong, setCurrentSong] = useState(
+    "https://songssongs.s3.ap-southeast-1.amazonaws.com/nadhif.mp3"
+  );
 
   const fetchData = async () => {
     const response = await fetch("/api/songs");
-    const songs = await response.json()
+    const songs = await response.json();
     setSongs(songs.rows);
   };
 
@@ -21,21 +23,25 @@ export default function Home() {
 
   const renderView = () => {
     return songs.map((song, index) => (
-      <div onClick={() => {
-        console.log(`${S3_BASE}/${song.audioUrl}`);
-        setCurrentSong(`${S3_BASE}/${song.audioUrl}`)}
-        
-        } key={`${song}-${index}`} className="flex w-1/4 flex-wrap m-2">
+      <div key={`${song}-${index}`} className="flex w-1/4 flex-wrap m-2">
         <div className="w-full p-1 md:p-6 bg-black bg-opacity-20 rounded-xl">
-          <Image
-            alt="gallery"
-            className="block h-4/5 w-full rounded-lg object-cover object-center"
-            src={`${S3_BASE}/${song.bannerUrl}`}
-            width={0}
-            height={0}
-            sizes="100vw"
-            priority
-          />
+          <a
+            href=""
+            onClick={(e) => {
+              e.preventDefault()
+              setCurrentSong(`${S3_BASE}/${song.audioUrl}`);
+            }}
+          >
+            <Image
+              alt="gallery"
+              className="block h-4/5 w-full rounded-lg object-cover object-center"
+              src={`${S3_BASE}/${song.bannerUrl}`}
+              width={0}
+              height={0}
+              sizes="100vw"
+              priority
+            />
+          </a>
           <h1 className="title text-xl mt-2">{song.title}</h1>
           <h3 className="albums">{song.album}</h3>
         </div>
@@ -48,10 +54,7 @@ export default function Home() {
       <div className="menu">
         <ul>
           <li>
-            <a
-              href="#"
-              className="hover:text-white text-white"
-            >
+            <a href="#" className="hover:text-white text-white">
               <Image
                 src="/home.svg"
                 alt="Search Logo"
@@ -64,10 +67,7 @@ export default function Home() {
             </a>
           </li>
           <li>
-            <a
-              href="#"
-              className="hover:text-white text-white"
-            >
+            <a href="#" className="hover:text-white text-white">
               <Image
                 src="/search.svg"
                 alt="Search Logo"
@@ -90,12 +90,7 @@ export default function Home() {
       </div>
       <div className="bottom-menu"></div>
       <div className="footer p-2 rounded-lg">
-        <ReactAudioPlayer
-          src={currentSong}
-          controls
-          loop
-          autoPlay
-        />
+        <ReactAudioPlayer src={currentSong} controls loop autoPlay />
       </div>
     </div>
   );
